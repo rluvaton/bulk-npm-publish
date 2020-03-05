1b89df36b6530dcc81a932cf150f5931ad912f1d
Raz Luvaton <rluvaton@gmail.com>
2020-03-05 16:34:15 +0200

    Add Release workflow that do #16 (publish to npm) and make the package run as a script #15

--- src/npm-publish-script-creator.ts
+++ src/npm-publish-script-creator.ts
@@ -1,11 +1,18 @@
-import {Package} from './storage-explorer';
+import { Package } from './storage-explorer';
 
 export interface NpmPublishOptions {
   registry?: string;
 }
 
-const npmPublishScriptCreator = (packages: Package[], options: NpmPublishOptions = {}): string[] => {
-  return packages.map((singlePackage) => `npm publish ${singlePackage.path} ${options.registry ? `--registry=${options.registry}` : ''}`.trim());
+const npmPublishScriptCreator = (
+  packages: Package[],
+  options: NpmPublishOptions = {}
+): string[] => {
+  return packages.map(singlePackage =>
+    `npm publish ${singlePackage.path} ${
+      options.registry ? `--registry=${options.registry}` : ''
+    }`.trim()
+  );
 };
 
 export default npmPublishScriptCreator;
