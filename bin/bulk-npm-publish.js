1dd144d35c932c6fc2db27b463f53b25d11485a8
Raz Luvaton <rluvaton@gmail.com>
2020-03-11 21:08:46 +0200

    fix(linux-running-library): fix cannot run on MacOS & Linux
    fix the error that in Linux & MacOS when running the package it will be
    executed as a shell script.
    thanks to @juanpicado
    
    Closes #33

---
+++ bin/bulk-npm-publish.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// This header added because of this issue #33
+// (otherwise it will be executed as a shell script (hence the errors).) @see https://github.com/rluvaton/bulk-npm-publish/issues/33#issuecomment-596917110
+// @see https://stackoverflow.com/a/34354713
+
+require('../dist/index.js');
