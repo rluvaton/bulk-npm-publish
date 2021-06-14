2b1dd64d6008b7beee963146923547074d680eb4
Raz Luvaton <rluvaton@gmail.com>
2021-06-14 03:27:58 +0300

    test: fix stack trace on error shown in wrong lines
    The solution was from:
    https://stackoverflow.com/a/48412190/5923666

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    verbose: true,
-    testMatch: [
-        "<rootDir>/src/**/*.spec.ts",
-        "<rootDir>/tests/**/*.spec.ts",
-    ],
-    restoreMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  testMatch: ['<rootDir>/src/**/*.spec.ts', '<rootDir>/tests/**/*.spec.ts'],
+  restoreMocks: true,
 };
